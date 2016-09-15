d22a4e4dac0d4832f807ebbd2735bb5738e537ea
Santiago O <san.9405@gmail.com>
2016-09-15 07:45:31 -0500

    Listar performers en la vista Studios. Plantilla

--- resources/views/Studio/listPerformers.blade.php
+++ resources/views/Studio/listPerformers.blade.php
@@ -1,2 +1,32 @@
 @extends('layouts.formularios')
 @section('content')
+    <link media="all" type="text/css" rel="stylesheet" href="../../public/media/css/studio.css">
+    <link media="all" type="text/css" rel="stylesheet" href="../../public/media/css/bootstrap.min.css">
+    <div class="container" >
+
+        <div class="col-md-12 border-container">
+
+            <div class="col-md-6 content" align="center" >
+                <img class= "resize" src="../../public/media/img/registrostudioperformer/caja1.png">
+                <div class="col-md-4 content" align="center">
+                    Oeeee
+
+                </div>
+            </div>
+
+            <div class="col-md-6 content" align="center">
+                <img class= "resize" src="../../public/media/img/registrostudioperformer/caja1.png">
+            </div>
+
+
+            <div class="col-md-6 content" align="center">
+                <img class= "resize" src="../../public/media/img/registrostudioperformer/caja1.png">
+
+            </div>
+
+            <div class="col-md-6 content" align="center">
+                <img class= "resize" src="../../public/media/img/registrostudioperformer/caja1.png">
+            </div>
+        </div>
+    </div>
+
